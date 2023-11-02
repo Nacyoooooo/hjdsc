70dce46612edb50fd381919410bbc8e8cec4fbfe
陈志豪 <12627264+czh_xia_tou_nan@user.noreply.gitee.com>
2023-11-02 14:11:24 +0800

    项目分包

---
+++ common/service-util/src/main/java/com/chenzhihao/serviceutil/ServiceUtilApplication.java
@@ -0,0 +1,13 @@
+package com.chenzhihao.serviceutil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceUtilApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceUtilApplication.class, args);
+    }
+
+}
---
+++ common/service-util/src/test/java/com/chenzhihao/serviceutil/ServiceUtilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chenzhihao.serviceutil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceUtilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
