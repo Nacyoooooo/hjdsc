14ea99b5dc8ddb5f33f188528251096574240984
陈志豪 <12627264+czh_xia_tou_nan@user.noreply.gitee.com>
2023-11-03 14:20:48 +0800

    实现用户登录，简陋版

---
+++ model/src/main/java/com/chenzhihao/model/Users.java
@@ -0,0 +1,272 @@
+package com.chenzhihao.model;
+
+
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * 
+ * @TableName users
+ */
+@TableName(value ="users")
+public class Users implements Serializable {
+    /**
+     * 主键，统一账号
+     */
+    @TableId
+    private Long id;
+
+    /**
+     * 姓名
+     */
+    private String name;
+
+    /**
+     * 密码
+     */
+    private String password;
+
+    /**
+     * 手机号
+     */
+    private String phonenumber;
+
+    /**
+     * 邮箱
+     */
+    private String email;
+
+    /**
+     * 头像的url地址
+     */
+    private String url;
+
+    /**
+     * 性别 1是男 2是女
+     */
+    private Integer gender;
+
+    /**
+     * 账号状态 1是正常 2是异常
+     */
+    private Integer status;
+
+    /**
+     * 账号创建时间
+     */
+    private Date createtime;
+
+    /**
+     * 账号更新时间
+     */
+    private Date updatetime;
+
+    @TableField(exist = false)
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 主键，统一账号
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * 主键，统一账号
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * 姓名
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * 姓名
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * 密码
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * 密码
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * 手机号
+     */
+    public String getPhonenumber() {
+        return phonenumber;
+    }
+
+    /**
+     * 手机号
+     */
+    public void setPhonenumber(String phonenumber) {
+        this.phonenumber = phonenumber;
+    }
+
+    /**
+     * 邮箱
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * 邮箱
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * 头像的url地址
+     */
+    public String getUrl() {
+        return url;
+    }
+
+    /**
+     * 头像的url地址
+     */
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    /**
+     * 性别 1是男 2是女
+     */
+    public Integer getGender() {
+        return gender;
+    }
+
+    /**
+     * 性别 1是男 2是女
+     */
+    public void setGender(Integer gender) {
+        this.gender = gender;
+    }
+
+    /**
+     * 账号状态 1是正常 2是异常
+     */
+    public Integer getStatus() {
+        return status;
+    }
+
+    /**
+     * 账号状态 1是正常 2是异常
+     */
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    /**
+     * 账号创建时间
+     */
+    public Date getCreatetime() {
+        return createtime;
+    }
+
+    /**
+     * 账号创建时间
+     */
+    public void setCreatetime(Date createtime) {
+        this.createtime = createtime;
+    }
+
+    /**
+     * 账号更新时间
+     */
+    public Date getUpdatetime() {
+        return updatetime;
+    }
+
+    /**
+     * 账号更新时间
+     */
+    public void setUpdatetime(Date updatetime) {
+        this.updatetime = updatetime;
+    }
+
+    @Override
+    public boolean equals(Object that) {
+        if (this == that) {
+            return true;
+        }
+        if (that == null) {
+            return false;
+        }
+        if (getClass() != that.getClass()) {
+            return false;
+        }
+        Users other = (Users) that;
+        return (this.getId() == null ? other.getId() == null : this.getId().equals(other.getId()))
+            && (this.getName() == null ? other.getName() == null : this.getName().equals(other.getName()))
+            && (this.getPassword() == null ? other.getPassword() == null : this.getPassword().equals(other.getPassword()))
+            && (this.getPhonenumber() == null ? other.getPhonenumber() == null : this.getPhonenumber().equals(other.getPhonenumber()))
+            && (this.getEmail() == null ? other.getEmail() == null : this.getEmail().equals(other.getEmail()))
+            && (this.getUrl() == null ? other.getUrl() == null : this.getUrl().equals(other.getUrl()))
+            && (this.getGender() == null ? other.getGender() == null : this.getGender().equals(other.getGender()))
+            && (this.getStatus() == null ? other.getStatus() == null : this.getStatus().equals(other.getStatus()))
+            && (this.getCreatetime() == null ? other.getCreatetime() == null : this.getCreatetime().equals(other.getCreatetime()))
+            && (this.getUpdatetime() == null ? other.getUpdatetime() == null : this.getUpdatetime().equals(other.getUpdatetime()));
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((getId() == null) ? 0 : getId().hashCode());
+        result = prime * result + ((getName() == null) ? 0 : getName().hashCode());
+        result = prime * result + ((getPassword() == null) ? 0 : getPassword().hashCode());
+        result = prime * result + ((getPhonenumber() == null) ? 0 : getPhonenumber().hashCode());
+        result = prime * result + ((getEmail() == null) ? 0 : getEmail().hashCode());
+        result = prime * result + ((getUrl() == null) ? 0 : getUrl().hashCode());
+        result = prime * result + ((getGender() == null) ? 0 : getGender().hashCode());
+        result = prime * result + ((getStatus() == null) ? 0 : getStatus().hashCode());
+        result = prime * result + ((getCreatetime() == null) ? 0 : getCreatetime().hashCode());
+        result = prime * result + ((getUpdatetime() == null) ? 0 : getUpdatetime().hashCode());
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", name=").append(name);
+        sb.append(", password=").append(password);
+        sb.append(", phonenumber=").append(phonenumber);
+        sb.append(", email=").append(email);
+        sb.append(", url=").append(url);
+        sb.append(", gender=").append(gender);
+        sb.append(", status=").append(status);
+        sb.append(", createtime=").append(createtime);
+        sb.append(", updatetime=").append(updatetime);
+        sb.append(", serialVersionUID=").append(serialVersionUID);
+        sb.append("]");
+        return sb.toString();
+    }
+}
